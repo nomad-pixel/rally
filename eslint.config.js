b74ba329e27093d888dbebf29fa34957099bd8b4
Nariman <onlanovnar@gmail.com>
2025-10-14 19:08:17 +0500

    setup prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,28 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ['dist'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
     },
   },
 ])
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 function App() {
-
   return (
-    <>
-    </>
+    <h1>
+      <div></div>
+    </h1>
   )
 }
 
