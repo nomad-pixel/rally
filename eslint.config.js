e61be22c0063570f50c9424a138e31419b0a2731
Nariman <onlanovnar@gmail.com>
2025-10-15 22:37:54 +0500

    add tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
-import prettier from 'eslint-plugin-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
   {
@@ -27,4 +27,4 @@ export default defineConfig([
       'prettier/prettier': 'error',
     },
   },
-])
+]);
